136bb64b21bea4314147a31daf266779fd8aa642
yashweblife <yashweblife20@gmail.com>
2023-04-09 17:03:38 -0500

    new auth system

--- server.py
+++ server.py
@@ -1,17 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request, redirect, url_for
 
+from helpers import create_user, validate_login, validate_signup
 app = Flask('__name__')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
+    if(request.method == 'POST'):
+        name= request.form['username']
+        password = request.form['password']
+        if(validate_login(name, password)):
+            return redirect(url_for('lists'))
     return render_template('login.html')
 
-@app.route('/signup')
+@app.route('/signup', methods=['POST', "GET"])
 def signup():
+    if(request.method == 'POST'):
+        name= request.form['username']
+        password = request.form['password']
+        if(validate_signup(name, password)):
+            create_user(name, password)
+            return redirect(url_for('lists'))
     return render_template('signup.html')
 
 @app.route('/sad_password')
