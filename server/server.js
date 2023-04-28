43af9f9f1a6d8ec9993d57849af54029243dc40f
yashweblife <yashweblife20@gmail.com>
2023-04-27 19:40:35 -0500

    use jsk and fix form inp

--- server/server.js
+++ server/server.js
@@ -3,14 +3,19 @@ const fs = require("fs")
 
 const app = express()
 app.use(express.static('public'))
-
+app.set('view engine', 'ejs');
 app.get('/',(req, res)=>{
     res.sendFile(__dirname+"/public/login.html")
 })
+
 app.get('/check_login',(req, res)=>{
-    console.log(req.body.username)
-    // res.end("<script>console.log("+req+")</script>");
-    res.end();
+    const {username, password} = req.query;
+    console.log({username, password})
+    if(username == "yash" && password == "1234"){
+        res.sendFile(__dirname+"/public/index.html", {username: username})
+    }else{
+        res.sendFile(__dirname+"/public/user_not_found.html")
+    }
 })
 
 const server = app.listen(8080,()=>{
